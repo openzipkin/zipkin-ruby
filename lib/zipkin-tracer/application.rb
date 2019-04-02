f8cb0e8f011130ac3989c32037990306b99f426f
Scott Steeg <ssteeg@mdsol.com>
2019-04-02 19:50:46 +0900

    Fallback to empty string as Rails 5.1 can't handle nil paths

--- lib/zipkin-tracer/application.rb
+++ lib/zipkin-tracer/application.rb
@@ -4,7 +4,7 @@ module ZipkinTracer
     # Determines if our framework knows whether the request will be routed to a controller
     def self.routable_request?(env)
       return true unless defined?(Rails) # If not running on a Rails app, we can't verify if it is invalid
-      path_info = env[ZipkinTracer::RackHandler::PATH_INFO]
+      path_info = env[ZipkinTracer::RackHandler::PATH_INFO] || ""
       http_method = env[ZipkinTracer::RackHandler::REQUEST_METHOD]
       Rails.application.routes.recognize_path(path_info, method: http_method)
       true
--- lib/zipkin-tracer/version.rb
+++ lib/zipkin-tracer/version.rb
@@ -1,3 +1,3 @@
 module ZipkinTracer
-  VERSION = '0.35.0'.freeze
+  VERSION = '0.35.1'.freeze
 end
